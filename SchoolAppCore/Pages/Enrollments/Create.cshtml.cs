faea7e1f0e185b5cc0fdbf0e62196c37e6e126b7
MileFisher <broken101284@gmail.com>
2025-04-21 15:36:27 +0700

    Add project files.

---
+++ SchoolAppCore/Pages/Enrollments/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SchoolAppCore.Models;
+
+namespace SchoolAppCore.Pages.Enrollments
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SchoolAppCore.Models.SchoolContext _context;
+
+        public CreateModel(SchoolAppCore.Models.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CourseId"] = new SelectList(_context.Courses, "CourseId", "CourseId");
+        ViewData["StudentId"] = new SelectList(_context.Students, "StudentId", "StudentId");
+            return Page();
+        }
+
+        [BindProperty]
+        public Enrollment Enrollment { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Enrollments.Add(Enrollment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
